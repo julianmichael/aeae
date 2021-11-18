55837777db8539a38b1b7b8040e8ae724e60a9bf
Julian Michael <julianjohnmichael@gmail.com>
2021-11-18 15:21:41 -0800

    Add tiny config file, fix predictor, add agreement, etc.

--- README.md
+++ README.md
@@ -12,5 +12,20 @@
 
 This project requires Python 3 and is written using AllenNLP and PyTorch.
 
-**Workstation setup:** start with `python scripts/download.py` from the base directory to download
-stuff, and install Python dependencies with `pip install -r requirements.txt`.
+**Workstation setup:**
+* Start with `python scripts/download.py` from the base directory to download
+datasets.
+* Install Python dependencies with `pip install -r requirements.txt`.
+* Preprocess datasets with `python scripts/build_data.py`.
+
+## Documentation
+
+NLI instances are preprocessed into the following format:
+```
+{
+  "uid": String,
+  "premise": String,
+  "hypothesis": String,
+  "label": "e" | "c" | "n" | "o"
+}
+```
--- aeae/predictors/text_classifier_with_distribution.py
+++ aeae/predictors/text_classifier_with_distribution.py
@@ -32,24 +32,14 @@ class TextClassifierWithDistributionPredictor(Predictor):
         Runs the underlying model and adds the `"label"` (maximum probability
         class) and `"label_distribution"` (full distribution) to the output.
         """
-        sentence = json_dict["sentence"]
+        premise = json_dict["premise"]
+        hypothesis = json_dict["hypothesis"]
         reader_has_tokenizer = (
             getattr(self._dataset_reader, "tokenizer", None) is not None
             or getattr(self._dataset_reader, "_tokenizer", None) is not None
         )
         if not reader_has_tokenizer:
             tokenizer = SpacyTokenizer()
-            sentence = tokenizer.tokenize(sentence)
-        return self._dataset_reader.text_to_instance(sentence)
-
-    @overrides
-    def predictions_to_labeled_instances(
-        self, instance: Instance, outputs: Dict[str, numpy.ndarray]
-    ) -> List[Instance]:
-        # TODO @margsli test this
-        new_instance = instance.duplicate()
-        label = numpy.argmax(outputs["probs"])
-        label_probs = outputs["probs"]
-        new_instance.add_field("model_label", LabelField(int(label), skip_indexing=True)) # TODO @margsli assumes the label is an int
-        new_instance.add_field("model_label_probs", LabelField(label_probs, skip_indexing=True))
-        return [new_instance]
+            premise = tokenizer.tokenize(premise)
+            hypothesis = tokenizer.tokenize(hypothesis)
+        return self._dataset_reader.text_to_instance(premise, hypothesis)
